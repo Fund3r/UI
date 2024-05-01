b26d78a780afb4041cde7af29b2a2252dd153019
harrychong <harryhuayuanchong@gmail.com>
2024-05-01 15:19:35 +0800

    feat: Header Route (Homepage & Explorer)

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -6,7 +6,10 @@ import Logo from '../../images/logos/app-logo.svg';
 import Name from '../../images/logos/app-name.svg';
 import Title from '../../images/logos/app-title.svg';
 
-export function Header() {
+export function Header({ pathName }: { pathName: string }) {
+  const navLinkClass = (path?: string) =>
+    path && pathName === path ? styles.navLink + ' underline' : styles.navLink;
+
   return (
     <header className="px-2 sm:px-6 lg:px-12 pt-3 pb-2 w-full">
       <div className="flex items-start justify-between">
@@ -15,6 +18,16 @@ export function Header() {
           <Image src={Name} width={130} alt="" className="hidden sm:block mt-0.5 ml-2" />
           <Image src={Title} width={210} alt="" className="mt-0.5 ml-2 pb-px" />
         </Link>
+
+        <nav className="sm:flex sm:space-x-8 py-2 flex items-center">
+          <Link href="/" className={navLinkClass('/')}>
+            Home
+          </Link>
+          <Link href="/explorer" className={navLinkClass('/explorer')}>
+            Explorer
+          </Link>
+        </nav>
+
         <div className="flex flex-col items-end md:flex-row-reverse md:items-start gap-2">
           <WalletControlBar />
         </div>
@@ -22,3 +35,8 @@ export function Header() {
     </header>
   );
 }
+
+const styles = {
+  navLink:
+    'flex items-center font-medium text-white tracking-wide hover:underline active:opacity-80 decoration-4 decoration-pink-500 underline-offset-[2px] transition-all',
+};
---
+++ src/pages/explorer.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const ExplorerPage: NextPage = () => {
+  return (
+    <div>Explorer</div>
+  );
+};
+
+export default ExplorerPage;
