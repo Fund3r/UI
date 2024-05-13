49c6bb4528793aad0cf5b3a8e6e01bfe6b0a7d82
harrychong <harryhuayuanchong@gmail.com>
2024-05-13 09:35:40 +0800

    feat: Create Project Form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,151 +1,155 @@
+import { SmallCloseIcon } from '@chakra-ui/icons';
 import {
+  Box,
   Button,
-  Heading,
-  PinInputField,
-  Radio,
+  Container,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  IconButton,
+  Image,
+  Input,
+  Select,
+  Stack,
+  Textarea,
   VStack,
-} from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
-import { AppCheckboxInput } from "../inputs/AppCheckboxInput";
-import { AppEditableInput } from "../inputs/AppEditableInput";
-import { AppInput } from "../inputs/AppInput";
-import { AppNumberInput } from "../inputs/AppNumberInput";
-import { AppPinInput } from "../inputs/AppPinInput";
-import { AppRadioInput } from "../inputs/AppRadioInput";
-import { AppRangeSliderInput } from "../inputs/AppRangeSliderInput";
-import { AppSelectInput } from "../inputs/AppSelectInput";
-import { AppSliderInput } from "../inputs/AppSliderInput";
-import { AppSwitchInput } from "../inputs/AppSwitchInput";
-import { AppTextareaInput } from "../inputs/AppTextareInput";
+  useToast
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
-type FormType = {
-  text: string;
-  checkbox: boolean;
-  editable: string;
-  number: number;
-  select: string;
-  switch: boolean;
-  textarea: string;
-  radio: string;
-  pinInput: string;
-  slider: string;
-};
+function CreateProjectForm() {
+  const { handleSubmit, register, control, setValue, getValues, watch, formState: { errors } } = useForm();
+  const toast = useToast();
+  const [logo, setLogo] = useState('');
+  const [projectImages, setProjectImages] = useState<string[]>([]);
 
-export default function Form() {
-  const {
-    handleSubmit,
-    control,
-    formState: { errors, isSubmitting },
-  } = useForm<FormType>();
+  
+  const onSubmit = (data) => {
+    toast({
+      title: "Project Created",
+      description: "Your project has been successfully created!",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });
+    console.log(data);
+  };
 
-  function onSubmit(values: any) {
-    console.log(values);
+  const handleImageUpload = (field) => (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        if (reader.result) { // 确保 reader.result 不为 null
+          const result = reader.result.toString();
+          if (field === 'logo') {
+            setLogo(result);
+            setValue('logo', result);
+          } else {
+            const newImages = [...projectImages, result];
+            if (newImages.length <= 3) {
+              setProjectImages(newImages);
+              setValue('projectImages', newImages);
+            }
+          }
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const removeImage = (index) => {
+    const newImages = projectImages.filter((_, i) => i !== index);
+    setProjectImages(newImages);
+    setValue('projectImages', newImages);
   }
 
+  useEffect(() => {
+    register('logo');
+    register('projectImages');
+  }, [register]);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <VStack spacing={5}>
-        <Heading>Create Project</Heading>
-        <AppInput<FormType>
-          control={control}
-          name="text"
-          label="Text Input Label"
-          helperText="Enter your text area"
-          errors={errors}
-        />
-        <AppNumberInput<FormType>
-          control={control}
-          name="number"
-          label="Quantity"
-          helperText="Enter your quantity"
-          errors={errors}
-        />
-
-        <AppCheckboxInput<FormType>
-          control={control}
-          name="checkbox"
-          label="Quantity"
-          errors={errors}
-          question="Do you like this?"
-        />
-        <AppSwitchInput<FormType>
-          control={control}
-          name="switch"
-          label="Quantity"
-          errors={errors}
-          question="Do you like this?"
-        />
-        <AppTextareaInput<FormType>
-          control={control}
-          name="textarea"
-          label="Quantity"
-          errors={errors}
-        />
-        <AppRangeSliderInput<FormType>
-          control={control}
-          name="editable"
-          label="Quantity"
-          errors={errors}
-        />
-        <AppEditableInput<FormType>
-          control={control}
-          name="editable"
-          label="Editable"
-          errors={errors}
-          defaultValue="This is editable text"
-        />
-        <AppSelectInput<FormType>
-          control={control}
-          name="select"
-          label="select input"
-          errors={errors}
-        >
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </AppSelectInput>
-
-        <AppRadioInput<FormType>
-          control={control}
-          name="radio"
-          label="s"
-          errors={errors}
-        >
-          <Radio value="option 1">Option 1</Radio>
-          <Radio value="option 2">Option 2</Radio>
-          <Radio value="option 3">Option 3</Radio>
-        </AppRadioInput>
-
-        <AppSliderInput<FormType>
-          control={control}
-          name="slider"
-          label="my slider"
-          errors={errors}
-        />
-
-        <AppPinInput<FormType>
-          control={control}
-          name="pinInput"
-          label="my pin"
-          errors={errors}
-        >
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-          <PinInputField />
-        </AppPinInput>
-
-        <Button
-          mt={4}
-          colorScheme="teal"
-          isLoading={isSubmitting}
-          type="submit"
-        >
-          Submit
-        </Button>
-      </VStack>
-    </form>
+    <Container maxW="container.md" mt={10}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <VStack spacing={5}>
+          <FormControl isInvalid={errors.projectName ? true : false}>
+            <FormLabel>Project Name</FormLabel>
+            <Input type="text" {...register("projectName", { required: "Project Name is required" })} />
+            <FormErrorMessage>{errors.projectName && typeof errors.projectName.message === 'string' ? errors.projectName.message : null}</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.tagLine ? true : false}>
+            <FormLabel>Tag Line</FormLabel>
+            <Controller
+              name="tagLine"
+              control={control}
+              rules={{ required: "Tag Line is required" }}
+              render={({ field }) => (
+                <Select placeholder="Select option" {...field}>
+                  <option value="defi">DeFi</option>
+                  <option value="nft">NFT</option>
+                  <option value="socialfi">SocialFi</option>
+                  <option value="gamefi">GameFi</option>
+                </Select>
+              )}
+            />
+            <FormErrorMessage>{errors.tagLine && typeof errors.tagLine.message === 'string' ? errors.tagLine.message : null}</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.description ? true : false}>
+            <FormLabel>Description</FormLabel>
+            <Textarea {...register("description", { required: "Description is required" })} />
+            <FormErrorMessage>{errors.description && typeof errors.description.message === 'string' ? errors.description.message : null}</FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Logo</FormLabel>
+            <Input type="file" accept="image/*" onChange={handleImageUpload('logo')} />
+            {logo && <Image src={logo} alt="Project Logo" boxSize="100px" mt={2} />}
+          </FormControl>
+          <FormControl>
+            <FormLabel>Project Images</FormLabel>
+            <Input type="file" accept="image/*" multiple onChange={handleImageUpload('projectImages')} />
+            <Flex wrap="wrap">
+              {projectImages.map((img, index) => (
+                <Box key={index} position="relative" m={2}>
+                  <Image src={img} alt={`Project Image ${index + 1}`} boxSize="100px" />
+                  <IconButton
+                    aria-label="Remove image"
+                    icon={<SmallCloseIcon />}
+                    position="absolute"
+                    right="0"
+                    top="0"
+                    size="sm"
+                    colorScheme="red"
+                    onClick={() => removeImage(index)}
+                  />
+                </Box>
+              ))}
+            </Flex>
+          </FormControl>
+          <FormControl isInvalid={errors.email ? true : false}>
+            <FormLabel>Email</FormLabel>
+            <Input type="email" {...register("email", { required: "Email is required" })} />
+            <FormErrorMessage>{errors.email && typeof errors.email.message === 'string' ? errors.email.message : null}</FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Owner</FormLabel>
+            <Input type="text" {...register("owner")} />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Social Links</FormLabel>
+            <Stack spacing={3}>
+              <Input type="text" placeholder="X link" {...register("socialLinks.x")} />
+              <Input type="text" placeholder="GitHub link" {...register("socialLinks.github")} />
+              <Input type="text" placeholder="Website" {...register("socialLinks.website")} />
+            </Stack>
+          </FormControl>
+          <Button type="submit" colorScheme="blue" size="lg">Create Project</Button>
+        </VStack>
+      </form>
+    </Container>
   );
 }
+
+export default CreateProjectForm;
